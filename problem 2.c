f0424d5e68631ee228f8e936899a8d42b2088a5b
taijul-islam-cs <taijul.islam.cs@gmail.com>
2025-06-24 00:09:55 +0600

    two number sum

---
+++ problem 2.c
@@ -0,0 +1,17 @@
+//Write a C program to enter two numbers and find their sum.
+#include<stdio.h>
+int main(){
+  int num1,num2,sum;
+
+ printf("enter first number:");
+ scanf("%d",&num1);
+
+ printf("enter second number:");
+ scanf("%d",&num2);
+
+  sum=num1+num2;
+ printf("sum is :%d",sum);
+
+return 0;
+}
+
